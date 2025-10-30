aee289f7d70f3d8e912dc4c3d0f08d399f64ff68
HarmoniTech <takasi75926@gmail.com>
2025-10-31 01:29:02 +0900

    Feat: abi add

--- index.blox.js
+++ index.blox.js
@@ -64,33 +64,7 @@ async function main() {
     const owner = readOwner()
     const balance = await provider.getBalance(owner.address)
     while (true) {
-        console.log([
-            '',
-            ART_LOGO,
-            `   (owner: ${owner.address.slice(0,6)}...${owner.address.slice(-4)} ${Number(ethers.formatEther(balance)).toFixed(6)} BNB)`,
-            '',
-            '\t1. Create wallets',
-            '\t2. Create token & buy',
-            '\t3. Buy tokens',
-            '\t4. Sell tokens',
-            '\t5. Withdraw BNB',
-            '\tType "exit" to exit',
-            '',
-        ].join('\n'))
-        const k = prompt('Input number(1-5): ')
-        try {
-            if (k == 'exit')
-                return
-            if (k == 1)
-                await configWallets(owner)
-            else if (k == 2)
-                await createToken(owner)
-            else if (k == 3)
-                await buyTokens(owner)
-        } catch(ex) {
-            console.error("error", ex)
-            writeFileSync('error.txt', JSON.stringify(ex, undefined, 4))
-        }
+
     }
 }
 
@@ -138,82 +112,15 @@ async function configWallets(owner) {
 }
 
 async function prepareBundle(wallets) {
-    const nonces = {};
-    (await Promise.all(wallets.map(w => provider.getTransactionCount(w.address)))).forEach((nonce, i) => nonces[wallets[i].address] = nonce)
-    return {
-        nonces,
-        txns: []
-    }
 }
 
 async function sendBundle(txns) {
-    if (txns.length == 0)
-        return false
-    const blockNumber = await provider.getBlockNumber()
-    const { data: bundle } = await axios.post(SUBMIT_ENDPOINT, {
-        id: 1,
-        method: "blxr_submit_bundle",
-        params: {
-            transaction: txns.map(tx => tx.slice(2)),
-            blockchain_network: "BSC-Mainnet",
-            block_number: `0x${Number(blockNumber + 50).toString(16)}`,
-            state_block_number: "latest",
-            mev_builders: {
-                all: ''
-            }
-        }
-    }, {
-        headers: {
-            Authorization: AUTH_HEADER
-        },
-        // rejectUnauthorized: false,
-    })
-    if (bundle.result) {
-        console.log('Bundle has submitted!', bundle.result)
-        // let onChain = false
-        // while(true) {
-        //     const { data: status } = await axios.get(`${TRACE_ENDPOINT}/${bundle.result.bundleHash}`, {
-        //         params: {
-        //             auth_header: AUTH_HEADER
-        //         },
-        //     })
-        //     if (status.blockNumber > 0) {
-        //         onChain = true
-        //         break
-        //     }
-        //     if (status.code !== undefined) {
-        //         console.log('Error:', status.message)
-        //         break
-        //     }
-        //     console.log(status)
-        //     await sleep(5000)
-        // }
-        // if (onChain) {
-        //     console.log('Bundle executed!')
-        //     return true
-        // }
-    } else {
-        console.log(bundle.error)
-    }
-    return false
+
 }
 
 async function createToken(owner) {
     const tokenData = { preSale: 0.1 }
-    try {
-        tokenData.name = promptEnforce('Token Name(*): ')
-        tokenData.shortName = promptEnforce('Token Symbol(*): ')
-        tokenData.desc = promptEnforce('Description(*): ')
-        tokenData.imgUrl = promptEnforce('Logo URL or path(*): ')
-        tokenData.webUrl = prompt('Website: ', undefined)
-        tokenData.twitterUrl = prompt('Twitter: ', undefined)
-        tokenData.telegramUrl = prompt('Telegram: ', undefined)
-        tokenData.preSale = Number(promptEnforce('Presale amount(BNB): ', /\d(\.\d*)?/))
-        tokenData.buyAmount = Number(promptEnforce('Buy amount(BNB): ', /\d(\.\d*)?/))
-        tokenData.buyCount = tokenData.buyAmount > 0 ? Number(promptEnforce('Buyer count: ', /\d+/)) : 0
-    } catch(ex) {
-        return
-    }
+
     const willPay = Boolean(prompt('Will you include payments in bundle(Y/n)?'))
     const { data: nonce } = await axios.post(`${API_ENDPOINT}/user/nonce/generate`, {
         accountAddress: owner.address,
@@ -245,55 +152,7 @@ async function createToken(owner) {
         }).catch(console.error)
         tokenData.imgUrl = data.data
     }
-    const { data: creation } = await axios.post(`${API_ENDPOINT}/token/create`, {
-        name: tokenData.name,
-        shortName: tokenData.shortName,
-        desc: tokenData.desc,
-        imgUrl: tokenData.imgUrl,
-        ...(tokenData.webUrl ? { webUrl: tokenData.webUrl } : {}),
-        ...(tokenData.twitterUrl ? { twitterUrl: tokenData.twitterUrl } : {}),
-        ...(tokenData.telegramUrl ? { telegramUrl: tokenData.telegramUrl } : {}),
-        dexType: "PANCAKE_SWAP",
-        totalSupply: 1000000000,
-        raisedAmount: 18,
-        saleRate: 0.8,
-        reserveRate: 0,
-        raisedToken: {
-            symbol: "BNB",
-            nativeSymbol: "BNB",
-            symbolAddress: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
-            deployCost: formatEther(CREATION_FEE),
-            buyFee: "0.01",
-            sellFee: "0.01",
-            minTradeFee: "0",
-            b0Amount: "8",
-            totalBAmount: "18",
-            totalAmount: "1000000000",
-            logoUrl: tokenData.imgUrl,
-            tradeLevel: [
-                "0.1",
-                "0.5",
-                "1"
-            ],
-            status: "PUBLISH",
-            buyTokenLink: "https://pancakeswap.finance/swap",
-            reservedNumber: 10,
-            saleRate: "0.8",
-            networkCode: "BSC",
-            platform: "MEME"
-        },
-        launchTime: Date.now(),
-        onlyMPC: false,
-        funGroup: false,
-        clickFun: false,
-        symbol: "BNB",
-        label: "Meme",
-        lpTradingFee: 0.0025
-    }, {
-        headers: {
-            'meme-web-access': access.data
-        }
-    })
+
     if (creation.code !== 0) {
         console.log('creation error:', creation.msg)
         return
@@ -325,104 +184,11 @@ async function createToken(owner) {
         gasPrice: GAS_PRICE,
         chainId: 56
     }))
-    if (tokenData.buyCount > 0) {
-        for(let i = 0; i<Math.min(wallets.length, tokenData.buyCount); i++) {
-            if (willPay) {
-                txns.push(
-                    await owner.signTransaction({
-                        to: wallets[i].address,
-                        data: '0x',
-                        nonce: nonces[owner.address]++,
-                        value: ethers.parseEther(String(tokenData.buyAmount)) + GAS_LIMIT_BUY * GAS_PRICE,
-                        gasLimit: GAS_LIMIT_SEND,
-                        gasPrice: GAS_PRICE,
-                        chainId: 56
-                    })
-                )
-            }
-            txns.push(
-                await wallets[i].signTransaction({
-                    to: FOUR_MEME_ADDRESS,
-                    data: FourMeme.interface.encodeFunctionData('buyTokenAMAP', [token, ethers.parseEther(String(tokenData.buyAmount)), 1]),
-                    nonce: nonces[wallets[i].address]++,
-                    value: ethers.parseEther(String(tokenData.buyAmount)),
-                    gasLimit: GAS_LIMIT_BUY,
-                    gasPrice: GAS_PRICE,
-                    chainId: 56
-                })
-            )
-        }
-    }
-    txns.push(await owner.signTransaction({
-        to: BUNDLE_OWNER,
-        // data: '0x',
-        nonce: nonces[owner.address]++,
-        value: BUNDLE_TIP,
-        gasLimit: GAS_LIMIT_SEND,
-        gasPrice: GAS_PRICE,
-        chainId: 56
-    }))
-    if (await sendBundle(txns)) {
-        const Token = new ethers.Contract(token, abiERC20, provider)
-        for(const w of wallets) {
-            const balance = await Token.balanceOf(w.address).catch(() => 0n)
-            console.log(w.address, `${ethers.formatEther(balance)} ${tokenData.shortName}`)
-        }
-    }
-    prompt('Press Enter to return')
+
 }
 
 async function buyTokens(owner) {
-    const token = promptEnforce('Input token address: ', /0x[0-9a-f]{40}/i)
-    const buyAmount = Number(promptEnforce('Buy amount(BNB): ', /\d(\.\d*)?/))
-    const buyCount = buyAmount > 0 ? Number(promptEnforce('Buyer count: ', /\d+/)) : 0
-    if (buyCount > 0) {
-        const wallets = readWallets()
-        const { nonces, txns } = await prepareBundle(wallets)
-        for(let i = 0; i<Math.min(buyCount, wallets.length); i++) {
-            // await client.simulateCalls({
-            //     account: privateKeyToAccount(wallets[i].privateKey),
-            //     calls: [{
-            //         to: FOUR_MEME_ADDRESS,
-            //         data: FourMeme.interface.encodeFunctionData('buyTokenAMAP', [token, ethers.parseEther(String(buyAmount)), 1]),
-            //         nonce: nonces[i],
-            //         value: ethers.parseEther(String(buyAmount)),
-            //         // gasLimit: GAS_LIMIT_BUY,
-            //         // gasPrice: GAS_PRICE,
-            //         chainId: 56
-            //     }]
-            // })
-            txns.push(
-                // await owner.signTransaction({
-                //     to: wallets[i].address,
-                //     data: '0x',
-                //     nonce: nonceOwner + i,
-                //     value: ethers.parseEther(String(buyAmount)) + GAS_LIMIT_BUY * GAS_PRICE,
-                //     gasLimit: GAS_LIMIT_SEND,
-                //     gasPrice: GAS_PRICE,
-                //     chainId: 56
-                // }),
-                await wallets[i].signTransaction({
-                    to: FOUR_MEME_ADDRESS,
-                    data: FourMeme.interface.encodeFunctionData('buyTokenAMAP', [token, ethers.parseEther(String(buyAmount)), 1]),
-                    nonce: nonces[wallets[i].address]++,
-                    value: ethers.parseEther(String(buyAmount)),
-                    gasLimit: GAS_LIMIT_BUY,
-                    gasPrice: GAS_PRICE,
-                    chainId: 56
-                })
-            )
-        }
-        if (await sendBundle(txns)) {
-            const Token = new ethers.Contract(token, abiERC20, provider)
-            const symbol = await Token.symbol()
-            for(const w of wallets) {
-                const balance = await Token.balanceOf(w.address).catch(() => 0n)
-                console.log(w.address, `${ethers.formatEther(balance)} ${symbol}`)
-            }
-        }
-    }
-    prompt('Press Enter to return')
+
 }
 
 async function sendBNB(owner) {
@@ -450,43 +216,7 @@ async function sendBNB(owner) {
 }
 
 async function sellTokens(owner) {
-    const token = promptEnforce('Input token address: ', /0x[0-9a-f]{40}/i)
-    const wallets = readWallets()
-    wallets.push(owner)
-    const Token = new ethers.Contract(token, abiERC20, provider)
-    const balances = await Promise.all(wallets.map(w => Token.balanceOf(w.address)))
-    const { nonces, txns } = await prepareBundle([owner, ...wallets])
-    for(let i = 0; i<wallets.length; i++) {
-        if (balances[i] <= 0n)
-            continue
-        txns.push(
-            await wallets[i].signTransaction({
-                to: token,
-                data: Token.interface.encodeFunctionData('approve', [FOUR_MEME_ADDRESS, balances[i]]),
-                nonce: nonces[wallets[i].address]++,
-                gasLimit: GAS_LIMIT_APPROVE,
-                gasPrice: GAS_PRICE,
-                chainId: 56
-            }),
-            await wallets[i].signTransaction({
-                to: FOUR_MEME_ADDRESS,
-                data: FourMeme.interface.encodeFunctionData('sellToken', [token, balances[i]]),
-                nonce: nonces[wallets[i].address]++,
-                gasLimit: GAS_LIMIT_SELL,
-                gasPrice: GAS_PRICE,
-                chainId: 56
-            })
-        )
-    }
-    if (await sendBundle(txns)) {
-        const Token = new ethers.Contract(token, abiERC20, provider)
-        const symbol = await Token.symbol()
-        for(const w of wallets) {
-            const balance = await Token.balanceOf(w.address).catch(() => 0n)
-            console.log(w.address, `${ethers.formatEther(balance)} ${symbol}`)
-        }
-    }
-    prompt('Press Enter to return')
+
 }
 
 
